6644b1e123965684d98bdb0af5cd726edff8e2e2
Mao <maoacr@gmail.com>
2021-12-27 00:16:56 -0500

    Taller de figuras geometricas, hallas area, perimetro, radio entre otras

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # curs-practico-javascript-platzi
 Desarrollo del Curso práctico de JavaScript impartido por JuanDC en la Escuelas de Desarrollo Web de Platzi 💚
+
+...
+
+## Taller #1: Figureas geometricas
+
+- Primer paso: Definir las fórmulas.
+- Segundo paso: Implementar las formulas en JavaScript.
+- Tercer paso: Crear funciones.
+- Cuarto paso: Integrar Js con HTML
---
+++ figuras.js
@@ -0,0 +1,54 @@
+//Codigo del cuadrado
+console.group("Cuadrado")
+const ladoCuadrado = 5;
+console.log("Los ladoas del cuadrdo miden " + ladoCuadrado + "cm cada uno");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es de " + perimetroCuadrado + " centimetros")
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es de " + areaCuadrado + " centimetros cuadrados")
+
+console.groupEnd()
+
+//Código para el triangulo
+console.group("Triangulo")
+
+const ladoUno =  6;
+const ladoDos = 6;
+const ladoTres = 4;
+const alturaTriangulo = 5.5;
+
+console.log("Los lados del triagulo miden: "+ ladoUno + " cms, " + ladoDos + " cms y " + ladoTres + " cms")
+
+const perimetroTriangulo = ladoUno + ladoDos + ladoTres;
+console.log("El perimetro del triangulo es de " + perimetroTriangulo + " centimetros")
+
+const areaTriangulo = (ladoTres*alturaTriangulo)/2
+console.log("El area del triangulo es de " + areaTriangulo + " cms cuadrados")
+console.groupEnd()
+
+//Código del circulo
+console.group("Circulo")
+
+//Radio
+const radioCirculo = 4;
+console.log("El radio del circulo es de " + radioCirculo + " cms" )
+
+//Diametro
+const diametroCirculo = radioCirculo * 2;
+console.log("El diametro del circulo es de " + diametroCirculo + " cms" )
+
+//PI
+const PI = Math.PI;
+console.log("PI equivale a " +  PI)
+
+//Circunferencia
+const circunferenciaCirculo = diametroCirculo * PI;
+console.log("La circunferencia del circulo es de " + circunferenciaCirculo + " cms" )
+
+//Area
+const areaCirculo = (radioCirculo*radioCirculo) * PI
+console.log("El area del circlo es de " + areaCirculo + " cms cuadrados" )
+
+console.groupEnd()
